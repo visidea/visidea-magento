41815eec478ac74b9e1543846f6afb82183d7120
Alessandro Rolando <ale@inferendo.com>
2026-05-29 09:25:34 +0200

    new javascript plugin version

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,5 +1,5 @@
 var config = {
     paths: {
-        'Visidea': '//cdn.visidea.ai/js-visidea/js-visidea-3.17.0.min'
+        'Visidea': '//cdn.visidea.ai/js-visidea/js-visidea-4.0.0.min'
     }
 };
